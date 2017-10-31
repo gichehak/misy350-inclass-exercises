3ee7bb280746665ebe978788a772666c4b0d5eed
gichehak <gichehak@udel.edu>
2017-10-31 10:45:58 -0400

    jquery

---
+++ 10-31-2017/jquery-basics.js
@@ -0,0 +1,22 @@
+function main(){
+  //document.getElementById('google').innerHTML = "Google" ;
+  $('#google').html("Google");
+  //let links = document.getElementsByClassName('my-link') ;
+  let links = $('.my-link');
+  links[0].innerHTML = "Twitter";
+  links[0].href = "http://www.twitter.com" ;
+  links[1].innerHTML = "TBD";
+  links[1].href = "#";
+
+  //hide and show main- text
+  $('#main-text').hide();
+  $('#main-text').fadeIn(3000);
+  $('.projects').hide();
+
+  //button click
+  $('.my-buttons').click(function() {
+    $(this).next().slideToggle(500);
+  });
+}
+
+$(document).ready(main);
